4aac9626039dbe2015684892b8ab131a18d3cc7c
Ahmad Suhail Qamil <suhailqamil@gmail.com>
2017-05-09 14:00:16 +0800

    Creating AdapterView

---
+++ app/src/main/java/com/fyp/masukami/weacon/AdapterHolder.java
@@ -0,0 +1,23 @@
+package com.fyp.masukami.weacon;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Suhail on 5/9/2017.
+ */
+
+public class AdapterHolder extends RecyclerView.ViewHolder {
+
+        TextView shopName, productName, shopLocation;
+        ImageView shopLogo;
+
+        public AdapterHolder(View itemView) {
+            super(itemView);
+            shopName = (TextView) itemView.findViewById(R.id.tvShopName);
+            productName = (TextView) itemView.findViewById(R.id.tvProductName);
+            shopLocation = (TextView) itemView.findViewById(R.id.tvLocation);
+        }
+}
--- app/src/main/java/com/fyp/masukami/weacon/AdapterShop.java
+++ app/src/main/java/com/fyp/masukami/weacon/AdapterShop.java
@@ -3,7 +3,14 @@ package com.fyp.masukami.weacon;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
 
 import java.util.Collections;
 import java.util.List;
@@ -30,16 +37,26 @@ public class AdapterShop extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.container_shop, parent, false);
+        AdapterHolder holder = new AdapterHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        AdapterHolder myHolder = (AdapterHolder) holder;
+        Advertisers current = data.get(position);
+        myHolder.shopName.setText(current.getName());
+        myHolder.productName.setText(current.getProductName());
+        myHolder.shopLocation.setText("Location : " + current.getAddress());
 
+        //load image using Glide
+        Glide.with(context).load("http://localhost/plazawalk/Advertisers/" + current.getName() + "/" + "logo.png");
     }
 
     @Override
     public int getItemCount() {
         return 0;
     }
+
 }
